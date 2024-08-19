94d916ed5a4444f884fe6c002a0ce2c41b18002d
Tughobo Ariaye <115959805+tughobo@users.noreply.github.com>
2024-08-19 20:17:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// function toggleMenu(){
+//     var menu = document.getElementById("menu");
+//     if (menu.style.display ==="block") {
+//         menu.style.display ="none"
+        
+//     }
+//     else{
+//         menu.style.display ="block";
+//     }
+
+// }
